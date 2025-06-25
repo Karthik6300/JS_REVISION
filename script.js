23c535d69d1a6c8dc1e42168cd402ee4ac825124
Nayani_Karthik <nayanikarthik961@gmail.com>
2025-06-25 12:18:33 +0530

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function generateOTP() {
+  let x = Math.random() * 1000000;
+  let otp = Math.floor(x);
+  if (otp < 1000000) {
+    otp += 1000000;
+    console.log("OTP:", otp);
+  } else {
+    console.log(otp);
+  }
+  return otp;
+}
+
+function showOTP() {
+  const otp = generateOTP();
+  document.getElementById("OTP").textContent = "generated OTP is : " + otp;
+}
+
+let str1 = 1 - 2 * 3 + (8 / 9) * 8 - 5 + 7 + 5;
+console.log(eval(str1));
