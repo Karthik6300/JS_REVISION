7c8a86433e8cbdb0fdb29fb618468427b59407a9
Nayani_Karthik <nayanikarthik961@gmail.com>
2025-06-25 12:44:40 +0530

    commit

--- script.js
+++ script.js
@@ -15,5 +15,12 @@ function showOTP() {
   document.getElementById("OTP").textContent = "generated OTP is : " + otp;
 }
 
-let str1 = 1 - 2 * 3 + (8 / 9) * 8 - 5 + 7 + 5;
-console.log(eval(str1));
+// let str1 = 1 - 2 * 3 + (8 / 9) * 8 - 5 + 7 + 5;
+// console.log(eval(str1));
+
+document.getElementById("div1").addEventListener("click", () => {
+  console.log("parent clicked");
+},false);
+document.getElementById("but1").addEventListener("click", () => {
+  console.log("child clicked");
+});
